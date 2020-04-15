5df94367947cdfd392fb273157f9b73dee9aba3d
René H <hasertrene@gmail.com>
2020-04-15 17:57:21 +0200

    Got rid of moment.js, and started with date-fns. now have an array of the month, with all values.

--- src/App.js
+++ src/App.js
@@ -1,84 +1,12 @@
 import React from "react";
-import Moment from "react-moment";
-import moment from "moment";
-import "./App.css";
+import "moment/locale/nl";
+import "./App.scss";
+import Calendar from "./pages/calendar";
 
 function App() {
-  const calendarStrings = {
-    lasthay: "[Yesterday at] LT",
-    sameDay: "[Today at] LT",
-    nexthay: "[Tomorrow at] LT",
-    lastWeek: "[last] dddd [at] LT",
-    nextWeek: "dddd [at] LT",
-    sameElse: "L",
-  };
-  const startWeek = moment().startOf("month").week();
-  const endWeek = moment().endOf("month").week() + 1;
-
-  let calendar = [];
-  for (var week = startWeek; week < endWeek; week++) {
-    calendar.push({
-      week: week,
-      days: Array(7)
-        .fill(0)
-        // eslint-disable-next-line
-        .map((n, i) =>
-          moment()
-            .week(week)
-            .startOf("week")
-            .clone()
-            .add(n + i, "day")
-        ),
-    });
-  }
-
-  const today = moment().format("DD");
-
-  console.log("week", moment().day());
-  console.log("days in the month", moment("2020-02", "YYYY-MM").daysInMonth());
-
   return (
     <div className='App'>
-      <h1>Camily</h1>
-      {moment().toString("MM", "YYYY")}
-      <table style={{ width: "100%", height: "100%" }}>
-        <thead style={{ backgroundColor: "#888", height: "100px" }}>
-          <tr>
-            <td>#</td>
-            <th>Sunday</th>
-            <th>Monday</th>
-            <th>Tuesday</th>
-            <th>Wednesday</th>
-            <th>Thursday</th>
-            <th>Friday</th>
-            <th>Saturday</th>
-          </tr>
-        </thead>
-        {calendar.map((week) => (
-          <tbody style={{ height: "100px" }}>
-            <tr>
-              <td className='week'>{week.week}</td>
-              {week.days.map((day) => (
-                <td>
-                  <span
-                    style={
-                      day.format("DD") === today
-                        ? {
-                            backgroundColor: "orange",
-                            borderRadius: "100%",
-                            margin: "-7%",
-                            padding: "7%",
-                          }
-                        : {}
-                    }>
-                    {day.format("DD")}
-                  </span>
-                </td>
-              ))}
-            </tr>
-          </tbody>
-        ))}
-      </table>
+      <Calendar />
     </div>
   );
 }
--- src/components/day.js
+++ src/components/day.js
@@ -1,19 +1,5 @@
 import React from "react";
 
-export default function day({ props }) {
-  return (
-    <span
-      style={
-        props.day.format("DD") === props.today
-          ? {
-              backgroundColor: "orange",
-              borderRadius: "100%",
-              margin: "-7%",
-              padding: "7%",
-            }
-          : {}
-      }>
-      {day.format("DD")}
-    </span>
-  );
+export default function day() {
+  return <></>;
 }
