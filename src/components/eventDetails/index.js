9e078bfb33ae8ce5fac6b12af34637f8cf0e07f4
René H <hasertrene@gmail.com>
2020-04-20 21:05:20 +0200

    Revert "updated the eventDetails form for editing"
    This reverts commit 47d3e9fb22c2184043346e1855389094ebce2301.
    
    Back to previous commit

--- src/components/eventDetails/index.js
+++ src/components/eventDetails/index.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { selectEventDetails } from "../../store/eventDetails/selectors";
 import { useSelector, useDispatch } from "react-redux";
-import { selectUser } from "../../store/user/selectors";
 import {
   updateEvent,
   postEvent,
@@ -11,10 +10,7 @@ import { Modal, Button, Form, Col, Row } from "react-bootstrap";
 import "../../styles/style.scss";
 
 export default function EventDetails(props) {
-  const [wholeDay, setWholeDay] = useState(false);
-  const user = useSelector(selectUser);
   const event = props.event;
-  console.log(user.members);
   return (
     <Modal
       {...props}
@@ -35,20 +31,8 @@ export default function EventDetails(props) {
           <Form.Group>
             <Form.Control type='date' value={event.date} />
           </Form.Group>
-          <Form.Group as={Row}>
-            <Col column sm='2'>
-              <Form.Label>Whole day</Form.Label>
-            </Col>
-            <Col column sm='2'>
-              <Form.Control
-                type='checkbox'
-                checked={!wholeDay}
-                onChange={() => setWholeDay(!wholeDay)}
-              />
-            </Col>
-            <Col column sm='4'>
-              {wholeDay && <Form.Control type='time' value={event.time} />}
-            </Col>
+          <Form.Group>
+            <Form.Control type='time' value={event.time} />
           </Form.Group>
           <Form.Group as={Row} controlId='exampleForm.ControlSelect1'>
             <Form.Label column sm='2'>
@@ -70,12 +54,11 @@ export default function EventDetails(props) {
             </Form.Label>{" "}
             <Col sm='10'>
               <Form.Control as='select'>
-                {user.members &&
-                  user.members.map((member) => (
-                    <option style={{ color: "green" }}>
-                      {member.firstName}
-                    </option>
-                  ))}
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
               </Form.Control>
             </Col>
           </Form.Group>
