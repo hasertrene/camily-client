055147dc546e8de954ba9fc12039684b24aa321d
René H <hasertrene@gmail.com>
2020-05-02 20:22:42 +0200

    api url

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,4 @@
-export const apiUrl = "https://camily-server.herokuapp.com";
-// "http://localhost:4000";
+export const apiUrl =
+  // "https://camily-server.herokuapp.com";
+  "http://localhost:4000";
 export const DEFAULT_MESSAGE_TIMEOUT = 3000;
